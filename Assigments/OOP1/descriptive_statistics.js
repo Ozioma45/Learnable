0e0559ebbe7b07f94d7d2ec3f0fa10a5c236f428
ozioma45 <oziomaegole@gmail.com>
2024-01-28 05:22:03 +0100

    completed the median function

--- Assigments/OOP1/descriptive_statistics.js
+++ Assigments/OOP1/descriptive_statistics.js
@@ -9,6 +9,17 @@ class DescriptiveStatistics {
     const sum = this.data.reduce((acc, value) => acc + value, 0);
     return sum / this.data.length;
   }
+
+  median() {
+    const sortedData = this.data.slice().sort((a, b) => a - b);
+    const middleIndex = Math.floor(sortedData.length / 2);
+
+    if (sortedData.length % 2 === 0) {
+      return (sortedData[middleIndex - 1] + sortedData[middleIndex]) / 2;
+    } else {
+      return sortedData[middleIndex];
+    }
+  }
 }
 
 // These are some examples
@@ -16,3 +27,4 @@ const data = [12, 15, 18, 22, 25, 25, 30, 35, 40, 42];
 const statsCalculator = new DescriptiveStatistics(data);
 
 console.log("Mean:", statsCalculator.mean());
+console.log("Median:", statsCalculator.median());
