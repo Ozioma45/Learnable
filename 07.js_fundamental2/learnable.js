760304d14cb6a4851d574306e42e7b512891356f
ozioma45 <oziomaegole@gmail.com>
2024-02-19 17:21:00 +0100

    reintroduction to Javascript Objects

---
+++ 07.js_fundamental2/learnable.js
@@ -0,0 +1,31 @@
+const myLearnableObject = { name: "Ernest", Age: 10 };
+
+const myLearnableObject2 = {
+  bestLanguage: "Javascript",
+  YearsOfExperience: 10,
+};
+
+const myFavoriteCar = {
+  firstCar: "Benz",
+  secondCar: "Toyota",
+};
+
+const bestProgrammingLangugge = new Object();
+
+bestProgrammingLangugge.firstLanguage = "Javascript";
+bestProgrammingLangugge.secondLanguage = "Python";
+
+console.log(bestProgrammingLangugge);
+
+/* console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E); */
+
+console.log(Math.sqrt(16));
+console.log(Math.pow(2, 4));
+console.log(Math.round(Math.PI));
+console.log(Math.ceil(Math.SQRT1_2));
