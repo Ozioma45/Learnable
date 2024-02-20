7f480e263cbe4b38d1310f6da3b59f19b226b884
ozioma45 <oziomaegole@gmail.com>
2024-02-20 01:18:33 +0100

    array and higher order functions

--- 07.js_fundamental2/learnable.js
+++ 07.js_fundamental2/learnable.js
@@ -1,3 +1,5 @@
+//Class 1: Reintroduction to Javascript
+
 const myLearnableObject = { name: "Ernest", Age: 10 };
 
 const myLearnableObject2 = {
@@ -15,7 +17,7 @@ const bestProgrammingLangugge = new Object();
 bestProgrammingLangugge.firstLanguage = "Javascript";
 bestProgrammingLangugge.secondLanguage = "Python";
 
-console.log(bestProgrammingLangugge);
+//console.log(bestProgrammingLangugge);
 
 /* console.log(Math.E);
 console.log(Math.PI);
@@ -25,7 +27,33 @@ console.log(Math.LN10);
 console.log(Math.LOG2E);
 console.log(Math.LOG10E); */
 
-console.log(Math.sqrt(16));
+/* console.log(Math.sqrt(16));
 console.log(Math.pow(2, 4));
 console.log(Math.round(Math.PI));
 console.log(Math.ceil(Math.SQRT1_2));
+ */
+
+//CLASS 2 : ARRAYS AND HIGHER ORDER FUNCTIONS
+
+const myFavoriteCars = ["Toyota", "Audi", "Benz"];
+
+/* console.log(myFavoriteCars);
+console.log(myFavoriteCars[0]);
+console.log(myFavoriteCars[1]);
+console.log(myFavoriteCars[2]);
+console.log(myFavoriteCars.length); */
+
+const learnableGrades = [50, 90, 80, 10, 20];
+
+/* const successfulCandidates = learnableGrades.filter((item) => item >= 50);
+
+console.log(successfulCandidates);
+ */
+/* const learnableGradesMultiplyByTwo = learnableGrades.map((item) => item * 2);
+console.log(learnableGradesMultiplyByTwo); */
+
+const sumOfLearnableGrade = learnableGrades.reduce(
+  (currentItem, accumulator) => currentItem + accumulator
+);
+
+console.log(sumOfLearnableGrade);
