3e3c779d6e037dc4e46eaa001c7575bcf6c657fc
Ozioma45 <oziomaegole@gmail.com>
2024-02-27 04:50:54 +0100

    constructor pattern

---
+++ 08.JavaScriptDesignPatterns/constructor.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+  }
+}
+
+//we can create new instances of the car
+let civic = new Car("Honda Civic", 2009, 20000);
+let mondeo = new Car("Ford Mondeo", 2010, 5000);
