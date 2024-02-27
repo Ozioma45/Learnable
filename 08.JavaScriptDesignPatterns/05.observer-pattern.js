a717674a010f5240f3bdd6ab6b567dc22f7157be
Ozioma45 <oziomaegole@gmail.com>
2024-02-27 05:42:29 +0100

    the observer pattern

---
+++ 08.JavaScriptDesignPatterns/05.observer-pattern.js
@@ -0,0 +1,24 @@
+class Subjest {
+  constructor() {
+    this.observers = new Set();
+  }
+
+  add(observer) {
+    this.observers.add(observer);
+  }
+
+  remove(observer) {
+    this.observers.delete(observer);
+  }
+
+  notify(context) {
+    for (let observer of this.observers) {
+      observer.update(context);
+    }
+  }
+}
+
+class Observer {
+  constructor() {}
+  update() {}
+}
