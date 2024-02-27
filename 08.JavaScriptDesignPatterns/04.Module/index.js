8edfef8ec1af8345832f8239979e0a866d907ecd
Ozioma45 <oziomaegole@gmail.com>
2024-02-27 05:31:40 +0100

    the module pattern

---
+++ 08.JavaScriptDesignPatterns/04.Module/index.js
@@ -0,0 +1,6 @@
+import { add, multiply, substract, square } from "./math.js";
+
+add(2, 3);
+substract(3, 2);
+square(5);
+multiply(2, 5);
---
+++ 08.JavaScriptDesignPatterns/04.Module/math.js
@@ -0,0 +1,17 @@
+// Math.js
+
+export function add(x, y) {
+  return x + y;
+}
+
+export function multiply(x) {
+  return x * 2;
+}
+
+export function substract(x, y) {
+  return x - y;
+}
+
+export function square(x) {
+  return x ** x;
+}
